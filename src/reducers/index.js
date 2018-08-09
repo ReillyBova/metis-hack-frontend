40b4e43a305406b2caf13595c5ab315dd12b4b60
Reilly Bova <rbova@princeton.edu>
2018-08-09 18:07:56 -0400

    Deployed with bug fixes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,19 +4,19 @@ export default (state = {active: [], inactive: services}, action) => {
   switch (action.type) {
     case 'ADD_SERVICE':
       const addedService = services.filter((service) => service.key === action.key)[0];
-      const filteredActive = state.active.filter((service) => service.key !== action.key)
-      const filteredInactive = state.inactive.filter((service) => service.key !== action.key)
+      const filteredActive1 = state.active.filter((service) => service.key !== action.key)
+      const filteredInactive1 = state.inactive.filter((service) => service.key !== action.key)
       return {
-        active: [...filteredActive, addedService],
-        inactive: filteredInactive
+        active: [...filteredActive1, addedService],
+        inactive: filteredInactive1
       }
     case 'REMOVE_SERVICE':
       const removedService = services.filter((service) => service.key === action.key)[0];
-      const filteredActive = state.active.filter((service) => service.key !== action.key)
-      const filteredInactive = state.inactive.filter((service) => service.key !== action.key)
+      const filteredActive2 = state.active.filter((service) => service.key !== action.key)
+      const filteredInactive2 = state.inactive.filter((service) => service.key !== action.key)
       return {
-        inactive: [...filteredInactive, removedService],
-        active: filteredActive
+        inactive: [...filteredInactive2, removedService],
+        active: filteredActive2
       }
     default:
       return state
