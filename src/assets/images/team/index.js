9e84fa5e7a4db5da6d10b6dc6f42f4b714575355
Reilly Bova <rbova@princeton.edu>
2018-08-09 14:59:40 -0400

    Finished about page

---
+++ src/assets/images/team/index.js
@@ -0,0 +1,3 @@
+export { default as reilly } from './reilly.jpg'
+export { default as aaron } from './aaron.jpg'
+export { default as kim } from './kim.jpg'
--- src/pages/about/about-page.jsx
+++ src/pages/about/about-page.jsx
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react'
-import { PageHeader, ComponentWrapper } from 'components'
-import { Table, Header, Rating} from 'semantic-ui-react'
+import { PageHeader, SectionHeader, ComponentWrapper } from 'components'
+import { Grid, Card, Header, Image } from 'semantic-ui-react'
 import { theme } from 'assets/theme'
+import { reilly, kim, aaron } from 'assets/images'
 import styled from 'styled-components'
 
 
@@ -11,8 +12,69 @@ class AboutPage extends Component {
     return (
       <Fragment>
         <PageHeader>{'About'}</PageHeader>
+        <SectionHeader>{'The Project'}</SectionHeader>
         <ComponentWrapper>
-          A website by Reilly Bova :)
+          <Card style={{width:'100%'}}>
+            <Card.Content><Header>Motivation</Header></Card.Content>
+            <Card.Content style={{fontSize: '18px'}}>
+              <p>”Monthly subscriptions are a key monetization channel
+                for legacy web, and arguably they are the most healthy
+                monetization channel for businesses on the legacy web
+                (especially when compared to ad/surveillance) based models.
+                They are arguably more healthy than a token based economic
+                system (depending upon the vesting model of the ICO) because...
+              </p>
+              <p>
+                For a User:
+                <ul>
+                  <li>You don’t have to read a complex whitepaper to use a dapps utility (as opposed to utility tokens)</li>
+                  <li>You don’t have to understand the founder’s vesting schedules</li>
+                  <li>You can cancel anytime on your own terms</li>
+                </ul>
+              </p>
+              <p>
+                For a dApp Service Provider:
+                <ul>
+                  <li>Since you know your subscriber numbers, churn numbers, conversion rate, you get consistent cash flow</li>
+                  <li>You get to focus on making your customers happy (as opposed to having two actors: speculators & users)</li>
+                </ul>
+              </p>
+
+                For these reasons, we think it’s creating a standard way to do ‘subscriptions’ on Ethereum.” - Kevin Owocki
+            </Card.Content>
+          </Card>
+        </ComponentWrapper>
+        <SectionHeader>{'The Team'}</SectionHeader>
+        <ComponentWrapper>
+          <Grid relaxed columns={3}>
+            <Grid.Column>
+              <Card>
+                <Image src={reilly} />
+                <Card.Content style={{fontSize: '18px'}}>
+                  Reilly Bova
+                  <Card.Description style={{fontSize: '14px'}}>Product Manager, Frontend Engineer</Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+                <Image src={kim} />
+                <Card.Content style={{fontSize: '18px'}}>
+                  Kim Macharia
+                  <Card.Description style={{fontSize: '14px'}}>Marketing, Business Development</Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+                <Image src={aaron} />
+                <Card.Content style={{fontSize: '18px'}}>
+                  Reilly Bova
+                  <Card.Description style={{fontSize: '14px'}}>Protocol Engineer</Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid>
         </ComponentWrapper>
       </Fragment>
     )
