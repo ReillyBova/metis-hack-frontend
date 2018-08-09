41067a69ac06e90c304f93b158c9000f2fa08469
Reilly Bova <rbova@princeton.edu>
2018-08-09 02:30:26 -0400

    Finished navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Navbar } from './components'
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='app-wrapper'>
         <Navbar />
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import logo from '../../assets/branding/metis.svg'
 import logoText from '../../assets/branding/metisText.svg'
+import $ from 'jquery';
 
 const NavbarWrapper = styled.header`
 width: 100%;
-height: 100%;
+height: 120px;
 position: sticky;
-position: -webkit-sticky;
 top: 0;
 z-index: 15;
 `
@@ -19,42 +19,109 @@ height: 120px;
 font-size: 24px;
 font-weight: 500;
 
--webkit-transition: height, font-size, 0.5s ease;
 transition: height, font-size, 0.5s ease;
 &.mini {
   font-size: 18px;
   height: 70px;
+
+
+  .navbar-brand-logo {
+    width: 38px!important;
+  }
+  .navbar-brand-text {
+    width: 176px!important;
+}
+`
+
+const NavbarBrand = styled.div`
+position: absolute;
+font-size: 24px;
+z-index: 12;
+
+display: flex;
+align-items: center;
+
+.navbar-brand-logo {
+  width: 88px;
+  padding: 16px;
+
+  transition: width, padding, 0.5s ease;
+}
+.navbar-brand-text {
+  width: 176px;
+  animation-name: fadeDown;
+
+  transition: width, padding, 0.5s ease;
+
+  animation-iteration-count: 1;
+  animation-duration: 2s;
+  animation-timing-function: ease;
+  animation-fill-mode: both;
+
+  @keyframes fadeDown {
+    0% { opacity: 0; transform: translateY(-50px); }
+    25% { opacity: 0; -webkit-transform: translateY(-50px); }
+    100% { opacity: 1; transform: translateY(0); }
+  }
+}
+`
+const NavbarLinks = styled.div`
+padding: 0 24px 0 300px;
+height: 100%;
+justify-content: flex-end;
+flex-flow: row wrap;
+align-items: center;
+display:flex;
+align-content: center;
+border-bottom: 3px solid #7600d9;
+`
+const NavbarLink = styled.div`
+margin: 4px 12px;
+border-bottom: 1px solid transparent;
+
+&:hover {
+  border-bottom: 1px solid #7600d9;
 }
 `
 
 class Navbar extends Component {
+
+  scroll_navbar = () => {
+    if ($(document).scrollTop() > 50) {
+      $('#navbarMain').addClass('mini');
+    } else {
+      $('#navbarMain').removeClass('mini');
+    }
+  }
+
+  componentDidMount() {
+    $(window).scroll(this.scroll_navbar);
+  }
   render() {
     return (
       <NavbarWrapper>
-        <NavbarMain>
-          <div class="navbar-brand">
-            <img src={logo} />
-            <img src={logoText} />
-          </div>
-          <div class="navbar-buttons">
-          </div>
-          <div class="navbar-links">
-            <div class="navbar-link">
+        <NavbarMain id={'navbarMain'}>
+          <NavbarBrand>
+            <img className={'navbar-brand-logo'} src={logo} />
+            <img className={'navbar-brand-text'} src={logoText} />
+          </NavbarBrand>
+          <NavbarLinks>
+            <NavbarLink>
               <a href="/account">{`My Account`}</a>
-            </div>
-            <div class="navbar-link">
+            </NavbarLink>
+            <NavbarLink>
               <a href="/browse-subscriptions">{`Add Subscription`}</a>
-            </div>
-            <div class="navbar-link">
+            </NavbarLink>
+            <NavbarLink>
               <a href="/billing">{`Billing History`}</a>
-            </div>
-            <div class="navbar-link">
+            </NavbarLink>
+            <NavbarLink>
               <a href="/about">{`About`}</a>
-            </div>
-            <div class="navbar-link">
+            </NavbarLink>
+            <NavbarLink>
               <a href="/logout">{`Logout`}</a>
-            </div>
-          </div>
+            </NavbarLink>
+          </NavbarLinks>
         </NavbarMain>
       </NavbarWrapper>
     )
