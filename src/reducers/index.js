c1d3699fba5e963dcd71a3b56dc8e768d14a9b82
Reilly Bova <rbova@princeton.edu>
2018-08-09 18:05:13 -0400

    Fixed logic in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,18 +3,20 @@ import services from 'assets/services'
 export default (state = {active: [], inactive: services}, action) => {
   switch (action.type) {
     case 'ADD_SERVICE':
-      const newService = services.filter((service) => service.key === action.key)[0];
-      const newInActive = state.inactive.filter((service) => service.key !== action.key)
+      const addedService = services.filter((service) => service.key === action.key)[0];
+      const filteredActive = state.active.filter((service) => service.key !== action.key)
+      const filteredInactive = state.inactive.filter((service) => service.key !== action.key)
       return {
-        active: [...state.active, newService],
-        inactive: newInActive
+        active: [...filteredActive, addedService],
+        inactive: filteredInactive
       }
     case 'REMOVE_SERVICE':
-      const newInService = services.filter((service) => service.key === action.key)[0];
-      const newActive = state.active.filter((service) => service.key !== action.key)
+      const removedService = services.filter((service) => service.key === action.key)[0];
+      const filteredActive = state.active.filter((service) => service.key !== action.key)
+      const filteredInactive = state.inactive.filter((service) => service.key !== action.key)
       return {
-        inactive: [...state.inactive, newInService],
-        active: newActive
+        inactive: [...filteredInactive, removedService],
+        active: filteredActive
       }
     default:
       return state
